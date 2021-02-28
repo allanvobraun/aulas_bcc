22d64d9415186782c4f818c1566869075ca26fa5
allanbraun <allanvobraun@gmail.com>
2021-02-28 16:14:16 -0300

    Response corrigida

--- src/discordBot/Response.ts
+++ src/discordBot/Response.ts
@@ -2,20 +2,26 @@ import {MessageOptions} from "discord.js";
 
 export default class Response {
 
-    content: string;
-    discordMessage: MessageOptions;
+    content: string | object;
+    formatedContent: string;
+    discordMessage: MessageOptions = {split: true};
 
     constructor(content: string | object, options?: MessageOptions) {
-        this.content = typeof content === 'string' ? content : this.toJson(content);
-        this.discordMessage = options ?? {content: this.content};
-        this.discordMessage.split = true;
+        this.content = content;
+        this.discordMessage = options ?? this.discordMessage;
+        this.formatedContent = '';
     }
 
     resolve(): MessageOptions {
+        this.formatedContent = typeof this.content === 'object' ?
+            this.toJsonString(this.content) :
+            this.content as string;
+
+        this.discordMessage.content = this.formatedContent;
         return this.discordMessage;
     }
 
-    protected toJson(content: object): string {
+    protected toJsonString(content: object): string {
         this.discordMessage.code = 'json';
         return JSON.stringify(content, null, 2);
     }
